cf80288b1f0779b1a6c7c7ca9363036c8c0df49e
YoungSi4 <dksdudgns01@gmail.com>
2026-04-07 21:59:47 +0900

    Mono Implementation

--- Assets/Scripts/Mono/MonoSpawner.cs
+++ Assets/Scripts/Mono/MonoSpawner.cs
@@ -1,16 +1,86 @@
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
 using UnityEngine;
+
+public class MonoSpawnerPool
+{
+    private Stack<MonoSweeper> sweeperPool = new Stack<MonoSweeper>();
+    public GameObject Prefab;
+
+    public void Init(int pool_size, GameObject prefab)
+    {
+        Prefab = prefab;
+
+        for (int i = 0; i < pool_size; i++)
+        {
+            AddObj();
+        }
+    }
+
+    public int PoolSize()
+    {
+        return sweeperPool.Count;
+    }
+
+    public void AddObj()
+    {
+        var temp = GameObject.Instantiate(Prefab).GetComponent<MonoSweeper>();
+        temp.enabled = false;
+        sweeperPool.Push(temp);
+    }
+
+    public void PushObj(MonoSweeper sweeper)
+    {
+        sweeperPool.Push(sweeper);
+    }
+
+    public MonoSweeper EnableObj()
+    {
+        if (sweeperPool.Count <= 0)
+        {
+            AddObj();
+        }
+
+        var sweeper = sweeperPool.Peek();
+        sweeperPool.Pop();
+        return sweeper;
+    }
+
+}
 
 public class MonoSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private MonoSpawnerPool SweeperPool;
+
+    public float Spawn_delay = 0.2f;
+    public float timer = 0f;
+    public int Spawn_count = 2;
+    public GameObject prefab;
+    public float Time_to_destroy = 30f;
+    public int initPoolSize = 100;
+
+    private void Awake()
     {
-        
+        SweeperPool = new MonoSpawnerPool();
+        SweeperPool.Init(initPoolSize, prefab);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer < Spawn_delay)
+            return;
+        timer = 0f;
+
+        MonoSweeper temp = SweeperPool.EnableObj();
+        temp.enabled = true;
+        temp.Init(transform.position, this);
+    }
+
+    public void PushSweeper(MonoSweeper sweeper)
+    {
+        SweeperPool.PushObj(sweeper);
+        sweeper.enabled = false;
     }
 }
--- Assets/Scripts/Mono/MonoSweeper.cs
+++ Assets/Scripts/Mono/MonoSweeper.cs
@@ -5,17 +5,27 @@ public class MonoSweeper : MonoBehaviour
     public float Speed = 40f;
     public float TimeToDestroy = 30f;
     public float Timer = 0f;
+    private MonoSpawner Spawner;
 
     void Update()
     {
         Timer += Time.deltaTime;
         if (Timer > TimeToDestroy)
         {
-            Destroy(gameObject);
-            return;
+            Spawner.PushSweeper(this);
         }
 
-
         transform.position = transform.position + new Vector3(Speed * Time.deltaTime, 0, 0);
+    }
+    
+    public void Init(Vector3 pos, MonoSpawner spawner)
+    {
+        Spawner = spawner;
+
+        var temp_pos = transform.position;
+        temp_pos.x = pos.x;
+        temp_pos.y = pos.y;
+        temp_pos.z = pos.z;
+        transform.position = temp_pos;
     }
 }
